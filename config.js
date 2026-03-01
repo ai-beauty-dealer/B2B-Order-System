cda471c0480eead6d591632642526bb1b7313ac1
分藤将徳 <bunchaca@bunfujishoutokunoMacBook-Air.local>
2026-03-01 17:10:34 +0900

    fix: Update GAS API URL to latest deployment

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 
 const CONFIG = {
     // GASのウェブアプリURLをここに設定します
-    API_URL: 'https://script.google.com/macros/s/AKfycbzLeYlmlbn2yBcWN_XVNi4JWWLdNABcfp2iswoV-Ux70fOBXVRRnyfQpiqhbI4a6YXF/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbymqNSfOKnGQDHunEqc-3R-X-3tSaVz6No4ApvW4B3xLky-6QiyEKxhtDkk0WiBTsn2/exec'
 };
