6823a81158242ae2ceb9b7616bf4628452c40685
分藤将徳 <bunchaca@bunfujishoutokunoMacBook-Air.local>
2026-03-02 11:33:11 +0900

    fix: Update GAS API URL to latest version (dynamic announcement banner enabled)

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 
 const CONFIG = {
     // GASのウェブアプリURLをここに設定します
-    API_URL: 'https://script.google.com/macros/s/AKfycbwjILuGOfJL_2rRO1pNkToWxD3VjIUQKWUhqTUibjMWozUxz86xsCmpo70WZ1gVZxbH/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbwf107CDMdL51caBoOjtQ6hCBL8XWjXjGsBJX1IvF9QfNUj3u0jye9quUJ7q0_SCjAR/exec'
 };
