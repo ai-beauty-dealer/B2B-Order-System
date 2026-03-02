cf44c2e17962137805078128f5dbc5f8d7e1dbee
分藤将徳 <bunchaca@bunfujishoutokunoMacBook-Air.local>
2026-03-02 11:23:28 +0900

    fix: Update GAS API URL to latest version (empty row filtering logic)

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 
 const CONFIG = {
     // GASのウェブアプリURLをここに設定します
-    API_URL: 'https://script.google.com/macros/s/AKfycbzqsPDfc9JTpNV6PFekqlfgkPRiSZ6Ymrwxtt6bf86Ui9YozUExgnIb7CJDGpndWy9R/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbwjILuGOfJL_2rRO1pNkToWxD3VjIUQKWUhqTUibjMWozUxz86xsCmpo70WZ1gVZxbH/exec'
 };
