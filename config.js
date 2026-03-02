e4be12cbef93b250dfcc098aa300f93c7da8c6e2
分藤将徳 <bunchaca@bunfujishoutokunoMacBook-Air.local>
2026-03-02 12:17:56 +0900

    fix: Update GAS API URL to latest version (concurrency support enabled)

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 
 const CONFIG = {
     // GASのウェブアプリURLをここに設定します
-    API_URL: 'https://script.google.com/macros/s/AKfycbzpq075kLHngo2PifWdBJWL6WqwyH0GFKT5pY-fcnPKdIZQaJx4zxBRDrZHP1SrVIxh/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbwim01jros05feBX_wnZCvWDUzXRQSKqLm42PjlpE28PmOmk2ZMwv0IYJB5FH8SIkg/exec'
 };
