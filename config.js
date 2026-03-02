81e6806ed715c0cb65012e0f72f3784227a1419c
分藤将徳 <bunchaca@bunfujishoutokunoMacBook-Air.local>
2026-03-02 11:13:37 +0900

    fix: Update GAS API URL to latest version (AI Secretary enabled)

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 
 const CONFIG = {
     // GASのウェブアプリURLをここに設定します
-    API_URL: 'https://script.google.com/macros/s/AKfycbz_tTePQixbTCHP5OLjHiQyvq5MPri6ZMUBOe3IO3Wj36fCJC8QrMKgD9wkjUPEFrpm/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbzqsPDfc9JTpNV6PFekqlfgkPRiSZ6Ymrwxtt6bf86Ui9YozUExgnIb7CJDGpndWy9R/exec'
 };
