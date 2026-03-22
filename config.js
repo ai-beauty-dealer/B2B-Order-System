0a4896bb61afd0de1cdf667479bbf70699798dc3
分藤将徳 <bunchaca@bunfujishoutokunoMacBook-Air.local>
2026-03-23 00:51:45 +0900

    chore: Restore original API_URL

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 
 const CONFIG = {
     // GASのウェブアプリURLをここに設定します
-    API_URL: 'https://script.google.com/macros/s/AKfycbxb5hnG8AbKdLoYe09GyAd98kOZdmUn2VVOPWrMcpw1b7SjXWPDqTM8qgrOYXz3HCfY/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbx_0JhXKADx4BRZcTtzv47e765b893Ld9P2wMHuih64ONKArFYVFz24Ppg7AVFT2diq/exec'
 };
